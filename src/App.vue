400d0117fa4afd5e4621816e20744c55e6fb4452
ashle <amireles1334@yahoo.com>
2023-03-10 01:25:06 -0600

    main page layout

--- src/App.vue
+++ src/App.vue
@@ -3,5 +3,18 @@
 </script>
 
 <template>
-  <RouterView />
+  <main>
+    <div class="card">
+      <RouterView />
+    </div>
+  </main>
 </template>
+
+<style lang="postcss" scoped>
+  main {
+    @apply h-screen flex justify-center items-center bg-[radial-gradient(ellipse_at_bottom,_var(--tw-gradient-stops))] from-blue-700 via-blue-800 to-gray-900;
+    & .card {
+      @apply flex h-5/6 min-h-fit w-11/12 flex-col items-center gap-16 overflow-auto rounded-lg bg-gray-100 p-8 shadow-lg;
+    }
+  }
+</style>
