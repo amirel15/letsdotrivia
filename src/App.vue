0673718f152da5bfc2bb635488071a191e8212e3
ashle <amireles1334@yahoo.com>
2023-03-11 12:20:57 -0600

    refactored & cleaned code

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,7 @@
-<script setup></script>
+<script setup>
+  import MainLayout from "@/components/MainLayout.vue"
+</script>
 
 <template>
-  <main>
-    <div class="card">
-      <RouterView />
-    </div>
-  </main>
-  <p class="score">Score: 100</p>
+  <MainLayout/>
 </template>
-
-<style lang="postcss" scoped>
-  main {
-    @apply h-screen flex justify-center items-center bg-[radial-gradient(ellipse_at_bottom,_var(--tw-gradient-stops))] from-blue-700 via-blue-800 to-gray-900;
-    & .card {
-      @apply flex h-5/6 min-h-fit w-11/12 flex-col items-center gap-16 overflow-auto rounded-lg bg-gray-100 p-8 shadow-lg;
-    }
-  }
-
-  .score {
-  @apply absolute bottom-6 right-8 text-2xl font-bold uppercase text-slate-200;
-}
-</style>
---
+++ src/components/MainLayout.vue
@@ -0,0 +1,21 @@
+<script setup>
+    import MainScore from "@/components/MainScore.vue"
+</script>
+
+<template>
+  <main>
+    <div class="card">
+      <RouterView />
+    </div>
+  </main>
+  <MainScore/>
+</template>
+
+<style lang="postcss" scoped>
+  main {
+    @apply h-screen flex justify-center items-center bg-[radial-gradient(ellipse_at_bottom,_var(--tw-gradient-stops))] from-blue-700 via-blue-800 to-gray-900;
+    & .card {
+      @apply flex h-5/6 min-h-fit w-11/12 flex-col items-center gap-16 overflow-auto rounded-lg bg-gray-100 p-8 shadow-lg;
+    }
+  }
+</style>
