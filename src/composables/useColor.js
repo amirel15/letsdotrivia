35464868c8155dcb5ba4d9af70f4c68c03c604f3
ashle <amireles1334@yahoo.com>
2023-03-11 22:09:33 -0600

    fixed color scheme

--- src/composables/useColor.js
+++ src/composables/useColor.js
@@ -1,5 +1,5 @@
 const useColor = () => {
-    const colors = ['bg-gray-500', 'bg-cyan-500', 'bg-cyan-500', 'bg-gray-500']
+    const colors = ['bg-indigo-800', 'bg-cyan-800', 'bg-blue-800', 'bg-sky-800']
     const getColor = (index) => colors[index % colors.length]
     return {getColor}
 }
